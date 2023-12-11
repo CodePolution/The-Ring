c1bb08dcf21543d99deb07b8b40ff2cf4e5280af
skeletus-design <nikita.voro24nin@gmail.com>
2023-12-11 22:21:47 +0300

    Co-authored-by: Platina1337 <Platina1337@users.noreply.github.com>

--- ring/polls/urls.py
+++ ring/polls/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import (
-    main
+    main,
+    Login
 )
 from . import views
 
 urlpatterns = [
-    path('', views.main, name='main')
+    path('', views.main, name='main'),
+    path('login/', Login.as_view(), name="log")
 ]
--- ring/polls/views.py
+++ ring/polls/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.views.generic import TemplateView
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 def main(request):
     return render(request, 'polls/main.html')
+
+class Login(TemplateView):
+    template_name = "polls/login.html"
+
+    def lgn(self, request):
+        if request.method == "POST":
+            llogin = request.POST.get("username")        
+            password = request.POST.get("password")    
+            user = authenticate(username=llogin, password=password)
+            
+            if user is not None:
+                login(request, user)
+                return redirect('main')
+                
+            else:
+                return redirect("login")            
