b0ddd586962026973eae21fd5a57e26b6dd182f1
skeletus-design <nikita.voro24nin@gmail.com>
2023-12-12 23:31:21 +0300

    Рабочий логин

--- ring/polls/views.py
+++ ring/polls/views.py
@@ -2,6 +2,7 @@ from django import forms
 from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
@@ -9,7 +10,7 @@ from django.contrib.auth.views import  LoginView
 from .forms import AuthorizationForm
 
 def main(request):
-    return render(request, 'polls/authorization.html')
+    return render(request, 'polls/main.html')
 
 
 class SignInView(LoginView):
@@ -17,13 +18,16 @@ class SignInView(LoginView):
     Вьюшка для входа пользователя в аккаунт
     """
 
-    template_name = 'account/auth/login.html'
+    template_name = 'polls/authorization.html'
     form_class = AuthorizationForm
     redirect_authenticated_user = True
-    success_url = settings.PROFILE_URL
+    success_url = reverse_lazy('main')
 
-    def dispatch(self, request, args, **kwargs):
+    def dispatch(self, request, *args, **kwargs):
         if self.request.user.is_authenticated:
             return redirect(self.success_url)
 
-        return super().dispatch(request,args, **kwargs)
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get_success_url(self) -> str:
+        return self.success_url
